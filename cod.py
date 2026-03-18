9d31b733fe1cf6d696c60ab2f0f45b2d41c23296
Carol <roxopereirakichcaroline@gmail.com>
2026-03-18 15:05:39 -0300

    2 commit

--- cod.py
+++ cod.py
@@ -1,11 +1,13 @@
 from random import randint
 num = randint(0, 5)
-print('Bem vindo ao jogo de adivinhação!')
+print('Bem vindo ao jogo de adivinhação do mago supremo!')
 chance = -1
 while chance!= num:
    chance = int(input('Adivinhe o número de 0 a 5 em que estou pensado:'))
    if chance == num:
          print ('Acertou! O número secreto era', num,'.')
+   elif chance >=6 :
+        print('Você colocou um número que não está na lista de opções! Toma um doce e tenta mais uma vez. ')     
    else:
           print ('Errado!Tente de novo.')
 print('Fim do jogo. Parabéns!')
