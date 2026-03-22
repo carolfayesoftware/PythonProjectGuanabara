6d4e59ee463ca2783e901e6411f16ce91626daf4
Carol <roxopereirakichcaroline@gmail.com>
2026-03-22 15:28:24 -0300

    feito

---
+++ ex31.py
@@ -0,0 +1,4 @@
+print('Então, parece que agora você quer ver que eu analise três números né?\n Vou organizar e lhe dizer qual é o maior e qual é o menor!')
+num1 = float(input('Digite o primeiro nùmero: '))
+num2 = float(input('Digite o segundo número: '))
+num3 = float(input('Digite o terceiro número:'))
