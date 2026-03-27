d3cc76e36657bda5923f585f9cbc7144230eda4b
Faye <roxopereirakichcaroline@gmail.com>
2026-03-26 22:12:58 -0300

    Update ex29.py

--- ex29.py
+++ ex29.py
@@ -3,6 +3,6 @@ veloc = float(input('Qual era a velocidade em que você estava dirigindo? '))
 if veloc > 80:
    multa =(veloc-80)*7
    limit = veloc-80
-   print('É, você vai ser multado em R${} por ter ido {}km/h acima da velocidade máxima permitida.'.format(multa, limit))
+   print('É, você vai ser multado em R${} por ter ido {}km/h acima da velocidade máxima permitida de 80km/h.'.format(multa, limit))
 else:
-   print('Parabéns, você não foi multado.')
+   print('Você não foi multado.Continue dirigindo com segurança!')
